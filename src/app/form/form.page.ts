bf3a57510a7262634a9eb3d66a8037f73b438e1a
WogLad <aaronbusiness2005@gmail.com>
2022-07-15 19:56:10 +0400

    made a bunch of changes idk about

--- src/app/form/form.page.ts
+++ src/app/form/form.page.ts
@@ -6,35 +6,41 @@ interface Task {
   type: string;
 }
 
-var taskList: Task[] = [
-  {
-    title: "First",
-    description: "The first task in the to-do list.",
-    type: "Work"
-  },
-  {
-    title: "Second",
-    description: "The second task in the to-do list.",
-    type: "Home"
-  },
-  {
-    title: "Test",
-    description: "This is just a test.",
-    type: "Other"
-  },
-];
-
 @Component({
   selector: 'app-form',
   templateUrl: './form.page.html',
   styleUrls: ['./form.page.scss'],
 })
 export class FormPage {
+  taskList: Task[] = [
+    {
+      title: "First",
+      description: "The first task in the to-do list.",
+      type: "Work"
+    },
+    {
+      title: "Second",
+      description: "The second task in the to-do list.",
+      type: "Home"
+    },
+    {
+      title: "Test",
+      description: "This is just a test.",
+      type: "Other"
+    },
+  ];
+  
+  taskTypes: string[] = [
+    "Home",
+    "Work",
+    "Other"
+  ];
+
   todo = {};
 
   parseTodoForm() {
     // console.log(this.todo);
-    taskList.push(this.todo as Task);
-    console.log(taskList);
+    this.taskList.push(this.todo as Task);
+    console.log(this.taskList);
   }
 }
